64bf4fb5c3785fba365f735ad1a245b8f0bfd931
Baptiste Honnay <honnaybaptiste@gmail.com>
2021-03-08 14:32:03 +0100

    add - Graph class and method signatures

---
+++ src/domain/Graph.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class Graph {
+
+  public Graph() {
+    super ();
+  }
+
+  public boolean addCountry(Country c) {
+    return false;
+  }
+
+  public boolean addBorder(Country c1, Country c2) {
+    return false;
+  }
+
+  public void calculerItineraireMinimisantNombreDeFrontieres(String from, String to, String fileName) {
+  }
+
+  public void calculerItineraireMinimisantPopulationTotale(String from, String to, String fileName) {
+  }
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,6 @@
 package main;
 
+import domain.Graph;
 import java.io.File;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
