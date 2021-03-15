f4eb65e15969b3d2a29cd6a48d4702fe41834983
Baptiste Honnay <baptiste.honnay@student.vinci.be>
2021-03-15 12:46:30 +0100

    new version for low pop

--- src/domain/Graph.java
+++ src/domain/Graph.java
@@ -9,11 +9,12 @@ public class Graph {
   private Map<String, List<String>> borders = new HashMap<String, List<String>>(); //cca3 -> borders
 
   public Graph() {
-    super ();
+    super();
   }
 
   /**
    * Adds a Country into the graph.
+   *
    * @param c : Country object
    * @return true if the Country has been added without issue (false otherwise)
    */
@@ -41,47 +42,47 @@ public class Graph {
     String currentCountry = from;                     // current position
 
     visited.add(currentCountry);
-    while(currentCountry!=null && !currentCountry.equals(to)) {
+    while (currentCountry != null && !currentCountry.equals(to)) {
       List<String> adjCountryLst = borders.get(currentCountry); // lst of adjacent countries
       adjCountryLst.sort(String::compareTo);
 
       for (String c : adjCountryLst) {
-        if(!visited.contains(c)){
+        if (!visited.contains(c)) {
           visited.add(c);
           fifo.addLast(c);
           parents.put(c, currentCountry);
         }
       }
-      if(visited.contains(to)){
+      if (visited.contains(to)) {
         currentCountry = to;
       } else {
-        if(fifo.peekFirst() != null) {
+        if (fifo.peekFirst() != null) {
           currentCountry = fifo.removeFirst();
-        }else {
+        } else {
           currentCountry = null;// null if empty fifo
         }
       }
     }
 
-    if(currentCountry == null) {
+    if (currentCountry == null) {
       System.out.println("Aucun chemin trouvé");
       return;
     }
 
     // load path into stack
     Deque<String> stack = new ArrayDeque<String>();
-    while(currentCountry != null){
+    while (currentCountry != null) {
       stack.push(currentCountry);
       currentCountry = parents.get(currentCountry);
     }
 
     // transfer stack (reverse) into list -> good order
     List<String> path = new ArrayList<>();
-    while(stack.size() != 0) {
+    while (stack.size() != 0) {
       path.add(stack.pop());
     }
 
-    for(String c : path) { //display path
+    for (String c : path) { //display path
       System.out.println(c + " ");
     }
   }
@@ -89,29 +90,29 @@ public class Graph {
   public void calculerItineraireMinimisantPopulationTotale(String from, String to, String fileName) {
     Map<Country, Long> finalTab = new HashMap<Country, Long>();
     SortedMap<Country, Long> provTab = new TreeMap<Country, Long>(Comparator.comparing((country -> country.getPopulation())));
-    //remplir tabs
-    for (Country c: countries) {
-      provTab.put(c, Long.MAX_VALUE);
-    }
+
     Country currentCountry = cca3ToCountry.get(from);
-    finalTab.put(currentCountry, (long) currentCountry.getPopulation());
     provTab.put(currentCountry, (long) currentCountry.getPopulation());
 
-    while (true) {
-      System.out.println(currentCountry.getCca3());
-      List<String> adjacentCountries = borders.get(currentCountry.getCca3());
-      long currentTotalPop = finalTab.get(currentCountry);
-      //to check which is the next country
-      for (String c: adjacentCountries) {
-        Country adjacentCountry = cca3ToCountry.get(c);
-        //do not do anything if already added in final tab
-        if (finalTab.containsKey(adjacentCountry))
-          continue;
-        long newAdjacentPopulation = currentTotalPop + adjacentCountry.getPopulation();
-        if (provTab.get(adjacentCountry) > newAdjacentPopulation)
-          provTab.put(adjacentCountry, newAdjacentPopulation);
+    while (!provTab.isEmpty() && !finalTab.containsKey(to)) {
+      finalTab.put(currentCountry, provTab.remove(currentCountry));
+      updateProv(currentCountry, provTab, finalTab);
+      if (!provTab.isEmpty()) {
+        currentCountry = provTab.firstKey();
       }
+    }
+    System.out.println(to + " : " + finalTab.get(cca3ToCountry.get(to)));
+  }
 
+  private void updateProv(Country currentCountry, SortedMap<Country, Long> provTab, Map<Country, Long> finalTab) {
+    long baseWeight = finalTab.get(currentCountry);
+    for (String cca3 : borders.get(currentCountry.getCca3())) {
+      Country c = cca3ToCountry.get(cca3);
+      long weight = baseWeight + c.getPopulation();
+      if (!finalTab.containsKey(c)) {
+        if (!provTab.containsKey(c) || weight < provTab.get(c))
+          provTab.put(c, weight);
+      }
     }
   }
 }
